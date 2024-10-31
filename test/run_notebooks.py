ae38287a56b64df71dafcc84c6057ee27aaaa611
Morgan Williams <morgan.williams@csiro.au>
2024-10-31 17:11:57 +1100

    Update notebook runner

--- test/run_notebooks.py
+++ test/run_notebooks.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import nbformat
 from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
 
-ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+ep = ExecutePreprocessor(timeout=600, kernel_name="sgtsg-workshop-env")
 
 # iterate through the notebooks, and catch cell exectuion errors to look at later
 exceptions = {}
